aeba4f3a75813acf56716bc1e60ca3b449dd2aed
Sammon Babu <sambmon@gmail.com>
2022-06-28 02:08:39 +0530

    Restaurant CRUD

--- foodvery-dashboard/src/App.js
+++ foodvery-dashboard/src/App.js
@@ -1,10 +1,11 @@
 import { Layout, Image } from "antd";
 import SideMenu from "../src/components/SideMenu";
 import AppRoutes from "./components/AppRoutes";
-import {Amplify} from 'aws-amplify';
+import { Amplify } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
-import awsconfig from './aws-exports';
+import awsconfig from "./aws-exports";
 import "@aws-amplify/ui-react/styles.css";
+import RestaurantContextProvider from "./contexts/RestaurantContext";
 
 const { Sider, Content, Footer } = Layout;
 
@@ -12,24 +13,26 @@ Amplify.configure(awsconfig);
 
 function App() {
   return (
-    <Layout>
-      <Sider style={{ height: "100vh", backgroundColor: "white" }}>
-        <Image
-          src="https://i.ibb.co/PYWxFqN/Red-and-Yellow-Food-Delivery-Service-Logo-845-475-px-1.png"
-          preview={false}
-          alt="FoodVery-Logo"
-        />
-        <SideMenu />
-      </Sider>
+    <RestaurantContextProvider>
       <Layout>
-        <Content>
-          <AppRoutes />
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          FoodVery Restaurant Dashboard ©2022
-        </Footer>
+        <Sider style={{ height: "100vh", backgroundColor: "white" }}>
+          <Image
+            src="https://i.ibb.co/PYWxFqN/Red-and-Yellow-Food-Delivery-Service-Logo-845-475-px-1.png"
+            preview={false}
+            alt="FoodVery-Logo"
+          />
+          <SideMenu />
+        </Sider>
+        <Layout>
+          <Content>
+            <AppRoutes />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            FoodVery Restaurant Dashboard ©2022
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
+    </RestaurantContextProvider>
   );
 }
 
--- foodvery-dashboard/src/components/SideMenu/index.js
+++ foodvery-dashboard/src/components/SideMenu/index.js
@@ -1,20 +1,21 @@
-import { Menu } from "antd";
+import { Menu, PageHeader } from "antd";
 import { useNavigate } from "react-router-dom";
 import { Auth } from "aws-amplify";
+import { useRestaurantContext } from "../../contexts/RestaurantContext";
 const SideMenu = () => {
   const navigate = useNavigate();
+  const { restaurant } = useRestaurantContext();
 
   const onClick = async (menuItem) => {
     if (menuItem.key === "signout") {
-     await Auth.signOut();
-     window.location.reload();
-
+      await Auth.signOut();
+      window.location.reload();
     } else {
       navigate(menuItem.key);
     }
   };
 
-  const menuItems = [
+  const mainMenuItems = [
     {
       key: "/",
       label: "Orders",
@@ -27,6 +28,10 @@ const SideMenu = () => {
       key: "order-history",
       label: "Order History",
     },
+  ];
+
+  const menuItems = [
+    ...(restaurant ? mainMenuItems : []),
     {
       key: "settings",
       label: "Settings",
@@ -38,7 +43,14 @@ const SideMenu = () => {
     },
   ];
 
-  return <Menu items={menuItems} onClick={onClick} />;
+  return (
+    <>
+      {restaurant && (
+        <PageHeader className="site-page-header" title={restaurant.name} />
+      )}
+      <Menu items={menuItems} onClick={onClick} />
+    </>
+  );
 };
 
 export default SideMenu;
